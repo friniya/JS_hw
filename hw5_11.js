8d83f2935da97060fd1d3632f26ec6f0c8d8d23c
friniya <friniya@gmail.com>
2024-05-23 10:04:57 +0200

    ДЗ 5.1. Ялинка

---
+++ hw5_11.js
@@ -0,0 +1,6 @@
+let j = "*";
+let k = " "
+for (var i = 0;i < 3; i++) {
+    k += j;
+    console.log(k);
+}
---
+++ hw5_12.js
@@ -0,0 +1,8 @@
+let i = 0;
+let j ="*";
+let k =" ";
+do {
+    k += j;
+    console.log(k);
+    i++;
+} while (i <= 5);
