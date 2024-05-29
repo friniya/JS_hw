a2caa1ab044f5827a5c6d6ca2155b9cdf32a9715
friniya <friniya@gmail.com>
2024-05-29 15:43:39 +0200

    ДЗ 6.1. Свій padEnd/padStart-second chans

---
+++ hw6_1.js
@@ -0,0 +1,15 @@
+ let str = 'qwerty';
+let sign = '*';
+let a = true;
+let b = (str.length) + 1;
+
+function pad(str, sign, a, b) {
+    switch (a) {
+        case true:
+            return sign.repeat(b - str.length)+str;
+        case false:
+            return str.padEnd(b, sign);
+    }
+}
+
+console.log(pad(str, sign, a, b));
