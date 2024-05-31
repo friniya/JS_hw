2cec1cfd1ccf64b670175da9a96202c3fabb88ae
friniya <43093501+friniya@users.noreply.github.com>
2024-05-31 12:14:41 +0200

    Update hw6_3.js
    внесла count

--- hw6_3.js
+++ hw6_3.js
@@ -1,9 +1,8 @@
-function number () {
+function number (count) {
 let evenCount = 0;
 let oddCount = 0;
 var answer = 0;
-let count = 21;
-for (let i = 0; i < count; i++)) {
+for (let i = 0; i < count; i++) {
 const randomNum = Math.floor(Math.random() * (1001 - 100)) + 100;
 if (randomNum % 2 === 0) {
 evenCount++;
@@ -13,4 +12,4 @@ oddCount++;
 }
 return answer = evenCount / oddCount;
 }
- console.log(number());
+ console.log(number(31));
