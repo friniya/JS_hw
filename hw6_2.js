f815d24d7332979997776506b77fc844a4b29da1
friniya <friniya@gmail.com>
2024-05-31 21:48:19 +0200

    ДЗ 6.2. Своя функція NaN

---
+++ hw6_2.js
@@ -0,0 +1,8 @@
+function isNaN (val){
+    let value =  ((+val + '') === 'NaN');
+    if (value = true){
+    return answer = "not NaN"
+    } else { return answer = NaN};
+}
+
+console.log(isNaN('abs'));
