7950679099fe6a250de247b7000b16cb94ce1041
friniya <43093501+friniya@users.noreply.github.com>
2024-05-31 07:32:20 +0200

    Update hw6_3.js
    поправила

--- hw6_3.js
+++ hw6_3.js
@@ -2,8 +2,8 @@ function number () {
 let evenCount = 0;
 let oddCount = 0;
 var answer = 0;
-
-for (let i = 0; i < 21; i++) {
+let count = 21;
+for (let i = 0; i < count; i++)) {
 const randomNum = Math.floor(Math.random() * (1001 - 100)) + 100;
 if (randomNum % 2 === 0) {
 evenCount++;
